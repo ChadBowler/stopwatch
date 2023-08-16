a62f0a410c67720496491bf92b604162faaee705
Chad Bowler <chadbowler@gmail.com>
2023-08-15 23:52:53 -0600

    added a reset button

--- stopwatch.py
+++ stopwatch.py
@@ -22,7 +22,7 @@ class App:
         self.timerLabel.place(relx=0.425, rely=0.1)
 
 
-        self.button = ctk.CTkButton(self.master,
+        self.start_button = ctk.CTkButton(self.master,
                                     fg_color='green',
                                     hover_color='darkgreen',
                                     text="Start",
@@ -32,7 +32,20 @@ class App:
                                     border_width=5,
                                     border_color='black',
                                     corner_radius=30)
-        self.button.place(relx=0.5, rely=0.6, anchor='center')
+        self.start_button.place(relx=0.3, rely=0.6, anchor='center')
+
+        self.reset_button = ctk.CTkButton(self.master,
+                                    fg_color='yellow',
+                                    hover_color='#F2B705',
+                                    text_color='black',
+                                    text="Reset",
+                                    font=('Arial bold', 28),
+                                    command=self.reset_timer,
+                                    width=300, height=100,
+                                    border_width=5,
+                                    border_color='black',
+                                    corner_radius=30)
+        self.reset_button.place(relx=0.7, rely=0.6, anchor='center')
 
     def timer(self):
         if self.running:
@@ -51,25 +64,31 @@ class App:
             self.miliseconds += 1
             self.timerLabel.configure(text=f"{'{:02d}'.format(self.hours)}:{'{:02d}'.format(self.minutes)}:{'{:02d}'.format(self.seconds)}:{'{:02d}'.format(self.miliseconds)}")
 
-
     def start_timer(self):
         self.start_time = int(time.time())
         self.running = True
-        self.button._fg_color='darkred'
-        self.button._hover_color='red'
-        self.button.configure(text='Stop')
-        self.button.configure(command=self.stop_timer)
+        self.start_button._fg_color='darkred'
+        self.start_button._hover_color='red'
+        self.start_button.configure(text='Stop')
+        self.start_button.configure(command=self.stop_timer)
+        self.reset_button._state='disabled'
         self.timer()
 
     def stop_timer(self):
         self.running = False
-        self.button._fg_color='green'
-        self.button._hover_color='darkgreen'
-        self.button.configure(text='Start')
-        self.button.configure(command=self.start_timer)
-        
-        
+        self.start_button._fg_color='green'
+        self.start_button._hover_color='darkgreen'
+        self.start_button.configure(text='Start')
+        self.start_button.configure(command=self.start_timer)
+        self.reset_button._state='normal'
 
+    def reset_timer(self):
+        self.miliseconds = 0
+        self.seconds = 0
+        self.minutes = 0
+        self.hours = 0
+        self.timerLabel.configure(text=f"{'{:02d}'.format(self.hours)}:{'{:02d}'.format(self.minutes)}:{'{:02d}'.format(self.seconds)}:{'{:02d}'.format(self.miliseconds)}")
+        
 if __name__=='__main__':
    app = ctk.CTk()
    gui = App(master=app)
